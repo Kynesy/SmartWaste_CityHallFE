61f2b3a1aff053b0ee693177f6ef8008c80ded9b
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-31 21:54:57 +0200

    implementato il componente pagamenti
    - l'user visualizza solo le proprie tasse ed ha la possibilità di pagarle
    - l'admin visualizza le tasse di tutti

--- src/app/components/payments/payments.component.ts
+++ src/app/components/payments/payments.component.ts
@@ -1,18 +1,92 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { Fee } from 'src/app/models/fee';
+import { TaxService } from 'src/app/services/tax.service';
 
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.component.html',
   styleUrls: ['./payments.component.scss']
 })
-export class PaymentsComponent implements OnInit{
+export class PaymentsComponent implements OnInit {
+  feeList: Fee[] = [];
+  year: number = new Date().getFullYear();
+  userRole: string | undefined;
+  userId: string | void | undefined;
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private authService: AuthService, private route: ActivatedRoute, private taxService: TaxService) {}
 
-  userRole: string | undefined
+  async ngOnInit(): Promise<void> {
 
-  ngOnInit(): void {
     this.userRole = this.route.snapshot.data['requiredRole'];
+  
+    if (this.userRole == 'ADMIN') {
+      this.getAdminData();
+    }
+  
+    if (this.userRole == 'USER') {
+      this.userId = await this.getUserID().catch(error => {
+        console.error("Error getting user ID: " + error);
+      });
+      if (this.userId) {
+        this.getUserData();
+      }
+    }
+
+  }
+
+  getAdminData(){
+    this.taxService.getAllTaxes().subscribe(
+      (fees: Fee[]) => {
+        this.feeList = fees;
+      },
+      (error: any) => {
+        console.error('Error retrieving taxes:', error);
+      }
+    );
+  }
+
+  getUserData(){
+    this.taxService.getAllUserTaxes(this.userId!).subscribe(
+      (fees: Fee[]) => {
+        this.feeList = fees;
+      },
+      (error: any) => {
+        console.error('Error retrieving taxes:', error);
+      }
+    );
+  }
+
+  getUserID(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.authService.user$.subscribe(
+        (profile: any) => {
+          const profileJson = JSON.stringify(profile, null, 2);
+          const profileData = JSON.parse(profileJson || "");
+          const subField = profileData['sub'] || '';
+          const subParts = subField.split('|');
+  
+          if (subParts.length === 2 && subParts[0] === 'auth0') {
+            resolve(subParts[1]); // Resolve the Promise with the user ID
+          } else {
+            reject("Invalid sub field format");
+          }
+        },
+        (error: any) => {
+          reject("Error while getting user profile: " + error);
+        }
+      );
+    });
+  }
+
+  payTax(taxId: string){
+    this.taxService.payTax(taxId).subscribe(
+      (response) => {
+        console.log(response);
+        this.feeList = [];
+        this.ngOnInit();
+      }
+    );
   }
 }
