a6104938af6b980444d1b63df9e1f70667d66696
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-31 21:54:00 +0200

    ora il logout pulisce lo storage sessione

--- src/app/components/auth/logout-button/logout-button.component.ts
+++ src/app/components/auth/logout-button/logout-button.component.ts
@@ -1,6 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-logout-button',
@@ -9,12 +10,13 @@ import { AuthService } from '@auth0/auth0-angular';
   ]
 })
 export class LogoutButtonComponent implements OnInit {
-  constructor(public auth: AuthService, @Inject(DOCUMENT) private doc: Document){}
+  constructor(public auth: AuthService, @Inject(DOCUMENT) private doc: Document, private storageService: StorageService){}
 
   ngOnInit(): void {
   }
 
   logout(): void{
+    this.storageService.clearData();
     this.auth.logout({ logoutParams: { returnTo: this.doc.location.origin}});
   }
 }
